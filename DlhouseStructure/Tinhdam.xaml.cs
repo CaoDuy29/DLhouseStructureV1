bf0223d86625c8dcb097d049d639022d2e2943d2
Caoduybmt <93959081+CaoDuy29@users.noreply.github.com>
2024-01-16 18:43:03 +0700

    up

---
+++ DlhouseStructure/Tinhdam.xaml.cs
@@ -0,0 +1,39 @@
+﻿using DlhouseStructure.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DlhouseStructure
+{
+    /// <summary>
+    /// Interaction logic for Tinhdam.xaml
+    /// </summary>
+    public partial class Tinhdam : Window
+    {
+        public Tinhdam()
+        {
+            InitializeComponent();
+            this.DataContext = new TinhDamViewModel();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DlhouseStructure/ViewModel/TinhDamViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DlhouseStructure.ViewModel
+{
+    public class TinhDamViewModel : BaseViewModel
+    {
+        public bool Isloaded = false;
+        public ICommand TinhToanDamCommand { get; set; }
+        public ICommand CanelDamCommand { get; set; }
+
+        private double _ChieuRongDam;
+        public double ChieuRongDam
+        {
+            get => _ChieuRongDam;
+            set
+            {
+                _ChieuRongDam = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public void SettingData()
+        {
+            ChieuRongDam = 300;
+
+        }
+
+        public TinhDamViewModel()
+        {
+            SettingData();
+
+            TinhToanDamCommand = new RelayCommand(ExecuteTinhToanDamCommand);
+            // Initialize other commands as needed, e.g., CanelDamCommand
+            // CanelDamCommand = new RelayCommand(ExecuteCanelDamCommand, CanExecuteCanelDamCommand);
+        }
+
+        private void ExecuteTinhToanDamCommand()
+        {
+            // Implement the action to display "Hello"
+            MessageBox.Show("Đã pick vào tính toán  ");
+        }
+
+    }
+}
